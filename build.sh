4c69a37beb5b3da1cb4b45bc38d6b06566e50ab4
greatestevermasters <greatestevermasters@gmail.com>
2025-09-30 21:29:38 +0530

    Add build script to run migrations

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit  
+
+pip install -r requirements.txt
+
+# Run migrations automatically on deploy
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
